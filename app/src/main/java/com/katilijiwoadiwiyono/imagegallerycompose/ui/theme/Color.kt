5b04bebeb1057f96d2c9eeea015e2015eaadc238
jiwomdf <mdf.jiwo@gmail.com>
2023-09-16 14:34:34 +0700

    first init

---
+++ app/src/main/java/com/katilijiwoadiwiyono/imagegallerycompose/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.katilijiwoadiwiyono.imagegallerycompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val md_theme_light_primary = Color(0xFFc00016)
+val md_theme_light_onPrimary = Color(0xFFffffff)
+val md_theme_light_primaryContainer = Color(0xFFffdad6)
+val md_theme_light_onPrimaryContainer = Color(0xFF410003)
+
+val md_theme_dark_primary = Color(0xFFffb4ac)
+val md_theme_dark_onPrimary = Color(0xFF690007)
+val md_theme_dark_primaryContainer = Color(0xFF93000e)
+val md_theme_dark_onPrimaryContainer = Color(0xFFffdad6)
+
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
