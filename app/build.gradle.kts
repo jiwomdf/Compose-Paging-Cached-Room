37f86d84c46f67925090352bb8cc0cf3417884f5
jiwomdf <mdf.jiwo@gmail.com>
2023-09-17 22:38:29 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,52 +59,44 @@ dependencies {
 
     api(project(":dependencies"))
     implementation(project(":core"))
-    implementation("androidx.paging:paging-common-ktx:3.2.1")
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-    implementation("androidx.paging:paging-compose:3.2.1")
 
-    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.7.2")
 
-    // Material Design 3
+    // Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+    implementation(composeBom)
     implementation("androidx.compose.material3:material3")
-
-    // Navigation Compose
-    implementation("androidx.navigation:navigation-compose:2.7.2")
-
-    // Optional - Integration with activities
     implementation("androidx.activity:activity-compose:1.7.2")
-    // Optional - Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    // Optional - Integration with LiveData
-    implementation("androidx.compose.runtime:runtime-livedata")
+
+    // Paging
+    implementation("androidx.paging:paging-common-ktx:3.2.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    implementation("androidx.paging:paging-compose:3.2.1")
+
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
 
     // Android Studio Preview support
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Coil
     implementation("io.coil-kt:coil-compose:2.4.0")
+
     // Accompanist
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
-
-    implementation("androidx.activity:activity-ktx:1.7.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(composeBom)
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -44,21 +44,21 @@ dependencies {
 
     api(project(":dependencies"))
 
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.48")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
-    //Room
+    // Room
     val roomVersion = "2.5.2"
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.room:room-paging:$roomVersion")
-
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
 
     testImplementation("junit:junit:4.13.2")
